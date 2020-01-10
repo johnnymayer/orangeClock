d9669d1b4aba9a551552f8279c7fe5e85ccd93dc
Johnny Mayer <johnnymayer@gmail.com>
2020-01-10 02:55:33 -0800

    Updates

---
+++ precache-manifest.7d85be3d3c95749b45e97c8d4fe8e328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15675f4097743f518e27b416e506859",
+    "url": "/orangeClock/index.html"
+  },
+  {
+    "revision": "245c5bad3b2202aaf6a4",
+    "url": "/orangeClock/static/css/main.901387e5.chunk.css"
+  },
+  {
+    "revision": "ba6a4154d21b9f236b06",
+    "url": "/orangeClock/static/js/2.aff4e1e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/orangeClock/static/js/2.aff4e1e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "245c5bad3b2202aaf6a4",
+    "url": "/orangeClock/static/js/main.c3ff7393.chunk.js"
+  },
+  {
+    "revision": "5afa584ac587c8cdd45e",
+    "url": "/orangeClock/static/js/runtime-main.df222924.js"
+  },
+  {
+    "revision": "4f55dbf84e69146105ba90e0e1dec1b1",
+    "url": "/orangeClock/static/media/orange.4f55dbf8.png"
+  }
+]);
