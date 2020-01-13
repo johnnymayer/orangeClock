3534775da4ff2b96dafc0506f1784ddeadbc60c4
Johnny Mayer <johnnymayer@gmail.com>
2020-01-12 20:53:52 -0800

    Slight CSS adjust

--- src/App.js
+++ src/App.js
@@ -4,19 +4,9 @@ import Clocker from './Clocker';
 import Orange from './orange.png';
 
 function App() {
-  const myStyle = {
-    backgroundImage: `url(${ Orange })`,
-    top: `0`,  
-    height: `100%`,
-    width: `100%`,
-    backgroundSize: `cover`,
-    backgroundRepeat: `no-repeat`,
-    backgroundPosition: `center`,
-
-  }
 
   return (
-    <div className="App" style={ myStyle }>
+    <div className="App" >
       <Clocker />
     </div>
   );
